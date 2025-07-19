e5390650588e460a17006bc97e00c29918b00a2b
andrewindeche <indecheandrew@gmail.com>
2025-07-19 13:02:39 +0300

    create debouncesearch custom hook

---
+++ src/hooks/useDebouncedSearch.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+function useDebouncedSearch(value: string, delay: number = 300) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebouncedSearch;
