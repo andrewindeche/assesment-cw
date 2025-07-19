8a2768116eae4c7c3f4f142299e7462ebefa141e
andrewindeche <indecheandrew@gmail.com>
2025-07-19 22:51:15 +0300

    change text color to gray

--- src/components/features/TagList.tsx
+++ src/components/features/TagList.tsx
@@ -8,14 +8,14 @@ type TagListProps = {
 
 export default function TagList({ title, tags, onTagClick }: TagListProps) {
   return (
-    <section className="mt-8 px-6 max-w-5xl mx-auto" aria-labelledby="tag-list section">
+    <section className="mt-8 px-9 max-w-5xl mx-auto" aria-labelledby="tag-list section">
       <h2 className="text-white text-lg font-semibold mb-4">{title}</h2>
       <div className="flex flex-wrap gap-3">
         {tags.map((tag, i) => (
           <Badge
             key={`${tag}-${i}`}
             onClick={() => onTagClick?.(tag)}
-            className="bg-gray-800 text-white hover:bg-gray-700 cursor-pointer transition duration-200"
+            className="bg-gray-800 text-gray-400 hover:bg-gray-700 cursor-pointer transition duration-200"
             aria-label={`Search tag: ${tag}`}
           >
             {tag}
