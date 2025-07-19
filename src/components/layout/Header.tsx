e127c6cfbb5a56b2d1a333d6e0bfd742cde34cb9
andrewindeche <indecheandrew@gmail.com>
2025-07-19 20:37:55 +0300

    adjust background and positioning of search icon

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
           <span>
             <span>
               <span className="absolute left-3 top-2.5">
-                <Search className="text-gray-400 text-lg" />
+                <Search className="absolute left-1 top-2.5 -translate-y-1/2 text-gray-400 w-6 h-6"/>
               </span>
             </span>
           </span>
@@ -23,10 +23,10 @@ function Header() {
             type="text"
             value="search"
             aria-label="header search input"
-            className="pl-12 w-40 h-10 bg-gray-800 text-white border-none focus:ring-0 rounded-md"
+            className="pl-12 w-40 h-10 bg-[#2b2e33] text-white border-none focus:ring-0 rounded-md"
           />
         </div>
-        <Avatar className="w-8 h-8">
+        <Avatar className="w-10 h-10">
           <AvatarImage src="/avatar.jpg" />
           <AvatarFallback>U</AvatarFallback>
         </Avatar>
